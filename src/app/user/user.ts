c6182803f74d2e599c9ed82846d2dae1a71daac7
barcelosrui <barcelos.rui@gmail.com>
2017-07-18 13:35:46 +0100

    change

---
+++ src/app/user/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    idUser:String;
+    name: string;
+    email: string;
+    street: string;
+    zipcode: string;
+    city: string;
+    contact: string;
+    nif: string;
+    username:string;
+	password:string;
+}
