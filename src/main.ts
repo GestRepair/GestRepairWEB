e089bd0b612e95970136dee4e531f431c2c6e74f
barcelosrui <barcelos.rui@gmail.com>
2017-07-25 20:22:51 +0100

    change password

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app/app.module';
 
 import { environment } from './environments/environment';
 
-if (environment.production) {
+if (!/localhost/.test(document.location.host)) {
   enableProdMode();
 }
 
