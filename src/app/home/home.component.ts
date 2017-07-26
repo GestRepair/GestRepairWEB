1c003ce8b16af4ccc566729a87acd9e9c688cc30
barcelosrui <barcelos.rui@gmail.com>
2017-07-26 20:57:41 +0100

    Update new template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ServiceService } from "app/service/service.service";
 import { Router } from "@angular/router";
 import { Service } from "app/service/service";
+declare var $;
 
 @Component({
   templateUrl: './home.component.html',
@@ -11,15 +12,38 @@ import { Service } from "app/service/service";
 export class HomeComponent {
   title = 'GestRepair';
   services: Service[];
-  
+
   constructor(private _serviceService: ServiceService, private router: Router) { }
   ngOnInit() {
     this.serv();
+    this.carousel();
   }
   serv() {
-	  this._serviceService.list().subscribe(
-		  services => this.services = services,
-		  error => console.log("Impossível carregar lista de Serviços")
-	  );
+    this._serviceService.list().subscribe(
+      services => this.services = services,
+      error => console.log("Impossível carregar lista de Serviços")
+    );
+  }
+  carousel() {
+    // Instantiate the Bootstrap carousel
+    $('.multi-item-carousel').carousel({
+      interval: false
+    });
+
+    // for every slide in carousel, copy the next slide's item in the slide.
+    // Do the same for the next, next item.
+    $('.multi-item-carousel .item').each(function () {
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+
+      if (next.next().length > 0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+      } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+      }
+    });
   }
 }
